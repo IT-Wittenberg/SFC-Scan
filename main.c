4c21f202f345170b34bc1af064206b60baaf92c1
Bruno <brunoscholz@gmx.de>
2024-02-26 13:17:28 +0100

    modified:   main.c 	modified:   sfc.log

--- main.c
+++ main.c
@@ -2,88 +2,43 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Global Variables
 const int globalDelay = 2; // set delay between next trys 
 const int maxTryOpenFile = 3; // open the file for writing try "maxTryOpenFile" times
-int programLimiter = 5; // define the Limit the Programm trys to scan the computer
-int isRunning = 0; // Bool expression that escape the While loop ( do not Change to 1 )
-int unexpectedExit = 0; // Bool yes or no | 0 or 1
-int counter = 0; // Counting
+const char * logfile = "C:\\Users\\bscholz\\My Stuff\\MyVSC\\GitHub\\SFC-Scann\\sfc.log";
+const char buffer[13]; // global Buffer used for getHostname {13 letter long }
+char hostname[sizeof(buffer)];
 
-void logAdd(char *text, char *host);
+
+void getHost(void);
+void logAdd(char *text, char host[sizeof(buffer)]);
+
 
 int main(void) {
-    // get client hostname
-    system("hostname > t.txt");
-    FILE *pFp = fopen("t.txt", "r");
-    char hostname[255]; // hold 1 Line
-    fgets(hostname, 255, pFp); // read line 1
-    fclose(pFp);
-    sleep(globalDelay);
-    system("del t.txt");
-
+    getHost();
     // scan section start
     logAdd("SFC Scan gestartet ...", hostname);
-
-    int programLimiter = 5; // define the Limit the Programm trys to scan the computer
-    int isRunning = 0; // Bool expression that escape the While loop ( do not Change to 1 )
-    int unexpectedExit = 0; // Bool yes or no | 0 or 1
-    int counter = 0; // Counting
-
-    // Pointer declaration
-    int *pProgramLimiter = &programLimiter, 
-    *pIsRunning = &isRunning, 
-    *pUnexpectedExit = &unexpectedExit, 
-    *pCounter = &counter;
-
-    while (*pIsRunning == 0 && *pCounter < *pProgramLimiter) {
-        int sfc_result = system("sfc /scannow");
-        //int sfc_result = 0;
-        int *pSfc_result = &sfc_result;
-        if (*pSfc_result == 0) {
-            // no damaged files found
-            if (*pCounter > 0) {
-                logAdd("Gefundene Integritätsverletzungen wurde repariert", hostname);
-            } else {
-                logAdd("Keine Integritätsverletzungen gefunden", hostname);
-            }
-            *pIsRunning ++;
-        } else if (*pSfc_result == 1) {
-            // damaged files found
-            if (*pCounter < *pProgramLimiter-1) {
-               logAdd("ETWAS IST SCHIEFGELAUFEN! Wird Scan erneut ausgeführt", hostname);
-            } else if (*pCounter == *pProgramLimiter-1) {
-                logAdd("Unerwarteter fehler. Abgebrochen", hostname);
-                *pIsRunning ++;
-                *pUnexpectedExit ++;
-            }
-        } else {
-            // unkown return value from scan
-            logAdd("ETWAS IST SCHIEFGELAUFEN! Unerwateter fehler", hostname);
-            *pIsRunning ++;
-            *pUnexpectedExit ++;
-            return -1;
-        }
-        *pCounter ++;
-    }
-
-    if (*pUnexpectedExit == 1) {
-        logAdd("PROGRAMM WURDE MIT UNERWARTETEN FEHLER BEENDET! Exitcode : 10", hostname);
-        return 10;
-    } else if (*pCounter == 5) {
-        logAdd("SCAN WURDE ABGEBROCHEN! LIMIT ERREICHT! Exitcode : 12", hostname);
-        return 12;
-    } else {
-        logAdd("Scan erfolgreich beendet. Exitcode : 0", hostname);
-        return 0;
-    }    
 }
 
-void logAdd(char *text, char *host) {
-    // Logfile handling
-    char *logfile = "\\\\stadtwbbaram\\dip$\\Apl\\Custom Programs\\Logs\\sfc_scan.log";
-    // char *logfile = getenv("LOGPATH");
+void getHost(void) {
+    // get client hostname
+    system("hostname > t.txt");
+    
+    char fileText[sizeof(buffer)];
+    FILE *fp;
+    fp=fopen("t.txt","r");
+    fgets(fileText,sizeof(buffer),fp);
+    fclose(fp);
 
+    system("del t.txt");;
+
+    strcpy(hostname, fileText);
+}
+
+void logAdd(char *message, char host[sizeof(buffer)]) {
+    // Logfile handling
     for(int i = 0; i < maxTryOpenFile; i++) {
         FILE *pFp = fopen(logfile, "a");
 
@@ -92,14 +47,15 @@ void logAdd(char *text, char *host) {
             printf("Error opening the file %s\n", logfile);
             // wait x secconds until next try
             sleep(globalDelay);
-            if (i == maxTryOpenFile-1) printf("Limit reached. No log entry created\n");
+            if (i == maxTryOpenFile-1) {
+                printf("Limit reached. No log entry created\n");
+            }
         } else {
             // set timestamp
             const time_t t = time(NULL);
             struct tm tm = *localtime(&t);
             // create log
-            fprintf(pFp, "%d-%02d-%02d %02d:%02d - { %s", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, host);
-            fprintf(pFp, "                           %s }\n", text);
+            fprintf(pFp, "%d-%02d-%02d %02d:%02d - { %s } - { %s }", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, host, message);
             fclose(pFp);
             i = maxTryOpenFile;
         }
