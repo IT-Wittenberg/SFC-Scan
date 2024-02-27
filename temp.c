b9d4a6f37d3a20c3d3e8290bf280da81dd675432
Bruno <brunoscholz@gmx.de>
2024-02-27 19:59:02 +0100

    modified:   temp.c

--- temp.c
+++ temp.c
@@ -2,11 +2,28 @@
 #include <time.h>
 #include <stdlib.h>
 
-char *getenv(const char *varname);
+const char buffer[10];
+
+int strcmp(char str1[sizeof(buffer)], char str2[sizeof(buffer)]);
 
 int main(void) {
-    printf("HOME : %s\n", getenv("HOME"));
+    char str1[sizeof(buffer)] = "ABCDEF";
+    char str2[sizeof(buffer)] = "ABCDEFG";
 
-    system("pause");
+    if (strcmp(str1, str2) == 1) {
+        printf("Stimmen nicht ueberein :(");
+    } else {
+        printf("Stimmen ueberein :)");
+    }
+
+    return 0;
+}
+
+int strcmp(char str1[sizeof(buffer)], char str2[sizeof(buffer)]) {
+    for (int i = 0; i < sizeof(buffer); i++) {
+        if (str1[i] != str2[i]) {
+            return 1;
+        }
+    }
     return 0;
 }
